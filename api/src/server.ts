3bde6fad4f506f45a7ca75a6775ce07d30f6e1f5
Vladimír Zdražil <mail@vzdrazil.com>
2020-07-18 12:10:51 +0200

    change lint rules

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,18 +1,18 @@
-import http = require("http");
-import url = require("url");
+import http = require("http")
+import url = require("url")
 
-const hostname = "127.0.0.1";
-const port = 3000;
+const hostname = "127.0.0.1"
+const port = 3000
 
 const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url, true).query;
-  console.log(queryObject);
-  const { userName } = queryObject;
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end(`Hello ${userName || "user"}! :D How are you? \n`);
-});
+  const queryObject = url.parse(req.url, true).query
+  console.log(queryObject)
+  const { userName } = queryObject
+  res.statusCode = 200
+  res.setHeader("Content-Type", "text/plain")
+  res.end(`Hello ${userName || "user"}! :D How are you? \n`)
+})
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
