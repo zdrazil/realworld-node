634a16320835da7929cc1afffce76f22ef9549e1
Vladimír Zdražil <mail@vzdrazil.com>
2020-07-18 12:08:59 +0200

    hello-world

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,16 @@
 import http = require("http");
+import url = require("url");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  const queryObject = url.parse(req.url, true).query;
+  console.log(queryObject);
+  const { userName } = queryObject;
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  res.end(`Hello ${userName || "user"}! :D How are you? \n`);
 });
 
 server.listen(port, hostname, () => {
